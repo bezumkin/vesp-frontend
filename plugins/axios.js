d57068fd469f82f3b3ec2e37b41c93c637708f74
bezumkin <bezumkin@yandex.ru>
2022-12-06 10:47:05 +0700

    feat: ability to use axios plugin without i18n

--- plugins/axios.js
+++ plugins/axios.js
@@ -7,6 +7,10 @@ export default ({app}) => {
     return config
   })
 
+  const $t = (msg) => {
+    return app.i18n ? app.i18n.t(msg) : msg
+  }
+
   app.$axios.interceptors.response.use(
     function (config) {
       return config
@@ -16,36 +20,36 @@ export default ({app}) => {
         switch (error.response.status) {
           case 422:
             if (error.response.data && typeof error.response.data === 'string') {
-              app.$toast.error(app.i18n.t(error.response.data))
+              app.$toast.error($t(error.response.data))
             }
             break
           case 401:
             if (app.$auth && app.$auth.loggedIn) {
               app.$auth.logout()
             } else {
-              app.$toast.error(app.i18n.t('errors.http401'))
+              app.$toast.error($t('errors.http401'))
             }
             break
           case 403:
           case 404:
           case 500:
           case 0:
-            app.$toast.error(app.i18n.t(`errors.http${error.response.status}`))
+            app.$toast.error($t(`errors.http${error.response.status}`))
             if (error.response.data && typeof error.response.data === 'string') {
               app.$toast.error(error.response.data)
             }
             break
           default:
             if (error.response.data && typeof error.response.data === 'string') {
-              app.$toast.error(app.i18n.t(error.response.data))
+              app.$toast.error($t(error.response.data))
             }
         }
       } else if (!error.status) {
         if (error.name !== 'ExpiredAuthSessionError') {
-          app.$toast.error(app.i18n.t('errors.http500'))
+          app.$toast.error($t('errors.http500'))
         }
       } else {
-        app.$toast.error(app.i18n.t('errors.http0'))
+        app.$toast.error($t('errors.http0'))
       }
 
       return Promise.reject(error.response)
